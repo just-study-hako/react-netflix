98a69a1028d45adbb622e97b1dbbb205db688c32
mineki <mineki94@naver.com>
2024-04-24 18:03:30 +0900

    영화 나열을 위한 Row 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import Banner from './components/Banner';
 import Nav from "./components/Nav";
+import requests from "./api/requests";
+import Row from "./components/Row";
 
 function App() {
   return <div className="App">
     <Nav />
     <Banner />
+    <Row
+      title="NETFLIX ORIGINALS"
+      id="NO"
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+    />
+
+    <Row 
+      title="Trending Nop"
+      id="TN"
+      fetchUrl={requests.fetchTrending}
+    />
+        <Row 
+      title="Top Rated"
+      id="TR"
+      fetchUrl={requests.fetchTopRated}
+    />
+        <Row 
+      title="Action Movies"
+      id="AM"
+      fetchUrl={requests.fetchActionMovies}
+    />
+        <Row 
+      title="Comedy Moveies"
+      id="CM"
+      fetchUrl={requests.fetchComendyMovies}
+    />
   </div>;
 };
 
