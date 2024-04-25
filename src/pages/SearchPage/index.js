2f6c16d3417569b55ccf3e0e11ae983e2676e862
mineki <mineki94@naver.com>
2024-04-25 19:28:48 +0900

    검색 영화 세부 페이지

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,11 @@
 import axios from '../../api/axios';
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import "./SearchPage.css";
 import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
+  const navigate = useNavigate();
   console.log('useLocation()', useLocation());
 
   const [searchResults, setSearchResults] = useState([]);
@@ -52,7 +53,7 @@ export default function SearchPage() {
               "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
             return (
               <div className='movie' key={movie.id}>
-                <div
+                <div onClick={() => navigate(`/${movie.id}`)}
                   className='movie__column-poster'
                 >
                   <img
